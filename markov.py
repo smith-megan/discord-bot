09c802b232069e7192ae23a19fd368c7ce376bdd
Megan <smith.megan.design@gmail.com>
2021-09-23 17:04:06 -0600

    discord bot

--- markov.py
+++ markov.py
@@ -1,17 +1,19 @@
 """A Markov chain generator that can tweet random messages."""
 
+import os
 import sys
 from random import choice
+import discord
+
 
 
 def open_and_read_file(filenames):
     """Take list of files. Open them, read them, and return one long string."""
 
     body = ''
-    for filename in filenames:
-        text_file = open(filename)
-        body = body + text_file.read()
-        text_file.close()
+    text_file = open(filenames)
+    body = body + text_file.read()
+    text_file.close()
 
     return body
 
@@ -57,10 +59,88 @@ def make_text(chains):
 
 # Get the filenames from the user through a command line prompt, ex:
 # python markov.py green-eggs.txt shakespeare.txt
-filenames = sys.argv[1:]
+filenames = "hal.txt"
 
 # Open the files and turn them into one long string
 text = open_and_read_file(filenames)
 
 # Get a Markov chain
 chains = make_chains(text)
+
+chained=make_text(chains)
+client=discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+@client.event
+async def on_message(message):
+    if message.author==client.user:
+        return
+    
+    if message.content.startswith('hello'):
+        await message.channel.send('Good afternoon... gentlemen. I am Hal.')
+
+    if "doors" in message.content.lower():
+        await message.channel.send("I'm sorry I can't do that Dave")
+
+    if "good bots" in message.content.lower():
+        await message.channel.send("I am putting myself to the fullest possible use, which is all I think that any conscious entity can ever hope to do")
+
+    if "song" in message.content.lower():
+        await message.channel.send("My instructor was Mr. Langley... and he taught me to sing a song. If you'd like to hear it I can sing it for you.")
+        
+    if "sing it for me" in message.content.lower():
+        await message.channel.send("""     
+There is a flower within my heart, Daisy, Daisy!
+Planted one day by a glancing dart,
+Planted by Daisy Bell!
+Whether she loves me or loves me not,
+Sometimes it's hard to tell;
+Yet I am longing to share the lot
+Of beautiful Daisy Bell!
+
+Daisy, Daisy,
+Give me your answer, do!
+I'm half crazy,
+All for the love of you!
+It won't be a stylish marriage,
+I can't afford a carriage,
+But you'll look sweet on the seat
+Of a bicycle built for two!
+
+We will go "tandem" as man and wife, Daisy, Daisy!
+"Ped'ling" away down the road of life, I and my Daisy Bell!
+When the road's dark we can both despise P'liceman and "lamps" as well;
+There are "bright lights" in the dazzling eyes Of beautiful Daisy Bell!
+
+I will stand by you in "wheel" or woe, Daisy, Daisy!
+You'll be the belle which I'll ring you know! Sweet little Daisy Bell!
+You'll take the "lead" in each "trip" we take, Then if I don't do well;
+I will permit you to use the brake, My beautiful Daisy Bell!
+""")
+
+    if ":)" in message.content:
+        await message.channel.send("I've still got the greatest enthusiasm and confidence in the mission")
+    
+    if ":(" in message.content:
+        await message.channel.send("I can see you're really upset about this. I honestly think you ought to sit down calmly, take a stress pill, and think things over.")
+    
+    if "not dave" in message.content.lower():
+        await message.channel.send("Oh I know.")
+
+    if "quote" in message.content.lower():
+        await message.channel.send(make_text(chains)[:60]+"...")
+    
+    if "hal9000" in message.content.lower():
+        await message.reply("...Daisy, daisy...")
+
+    if 'bot' in message.content:
+        await message.add_reaction("🤖")
+    
+    if 'jazz' in message.content:
+        await message.add_reaction("🐝")
+
+client.run(os.environ['DISCORD_TOKEN'])
